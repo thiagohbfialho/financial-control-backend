056aae108ecd7d5352ce8abc88f1843744981088
Thiago Fialho <thiago.fialhoti@gmail.com>
2022-05-20 16:54:42 -0300

    Adicionando o endpoint summary e criando um tratamento de erros

---
+++ src/api/common/errorHandler.js
@@ -0,0 +1,14 @@
+module.exports = (req, res, next) => {
+    const bundle = res.locals.bundle
+
+    if(bundle.errors){
+        const errors = parseErrors(bundle.errors)
+        res.status(500).json({errors})
+    }else{
+        next()
+    }
+}
+
+const parseErrors = (nodeRestfulErrors) => {
+    const errors = []
+}
--- src/api/financialControl/financialControlService.js
+++ src/api/financialControl/financialControlService.js
@@ -23,4 +23,29 @@ FinancialControl.route('count', (req, res, next) => {
     })
 })
 
+FinancialControl.route('summary', (req, res, next) => {
+    FinancialControl.aggregate([{ 
+        $project: {
+            provento: {$sum: "$proventos.valor"}, 
+            pagamento: {$sum: "$pagamentos.valor"},
+            desconto: {$sum: "$descontos.valor"}
+        } 
+    }, { 
+        $group: {
+            _id: null, 
+            provento: {$sum: "$provento"}, 
+            pagamento: {$sum: "$pagamento"}, 
+            desconto: {$sum: "$desconto"}
+        }
+    }, { 
+        $project: {_id: 0, provento: 1, pagamento: 1, desconto: 1}
+    }], (error, result) => {
+        if(error) {
+            res.status(500).json({errors: [error]})
+        } else {
+            res.json(result[0] || {provento: 0, pagamento: 0, desconto: 0})
+        }
+    })
+})
+
 module.exports = FinancialControl
