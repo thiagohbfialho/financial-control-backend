35801869751a74ddd4a57e24a2c4265b25a3069e
thiagohbfialho <thiago.fialhoti@gmail.com>
2022-05-22 12:56:43 -0300

    Adicionando o express-query-int para fazer pesquisas com filtros

--- src/config/server.js
+++ src/config/server.js
@@ -3,10 +3,12 @@ const port = 3003
 const express = require('express')
 const server  = express()
 const allowCors = require('./cors')
+const queryParser = require('express-query-int')
 
 server.use(express.urlencoded({ extended: true }))
 server.use(express.json())
 server.use(allowCors)
+server.use(queryParser())
 
 server.listen(port, function() {
     console.log(`BACKEND is running on port ${port}`)
